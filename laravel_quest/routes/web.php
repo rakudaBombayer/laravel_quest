cc727b120f20e356233281a69c34ccdebc7991fc
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-01-05 03:29:11 +0900

    ルートを設定しました。

--- laravel_quest/routes/web.php
+++ laravel_quest/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () { return view('home'); })->name('home');
+
+Route::get('/article/create', function () { return view('article.create'); })->name('article.create');
+
+Route::get('/article/edit', function () { return view('article.editarticle'); })->name('article.edit');
+
+Route::get('/article', function () { return view('article.article'); })->name('article');
